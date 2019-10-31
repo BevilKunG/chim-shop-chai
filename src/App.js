ca1f3ea7bed7f8150318b81614ec2527c50c36ad
parinya <devilbevilgamer@gmail.com>
2019-10-31 16:05:39 +0700

    [EDITED] set up project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import Header from './components/Header'
+import Banner from './components/Banner'
+import Duration from './components/Duration'
+import Detail from './components/Detail'
+import CallLogo from './components/CallLogo'
+import Footer from './components/Footer'
+import './components/App.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    jsonData: null
+  }
+
+  componentDidMount() {
+    axios.get('https://panjs.com/ywc.json')
+      .then(({ data }) => {
+        // console.log(data);
+        this.setState({ jsonData: data })
+        // console.log(this.state);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+      <Header data={ this.state.jsonData }/>
+      <Banner/>
+      <Duration data={ this.state.jsonData }/>
+      <Detail data={ this.state.jsonData }/>
+      <CallLogo />
+      <Footer data={ this.state.jsonData }/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
